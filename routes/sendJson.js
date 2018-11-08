0cd3a8313a2042cf60eb2db9a65d1f506809112c
WonBeomChoi <cwb2310@gmail.com>
2018-11-08 20:27:48 +0900

    lastpush?

--- routes/sendJson.js
+++ routes/sendJson.js
@@ -12,33 +12,43 @@ router.all('/', function(req, res, next) {
     
     // var message = JSON.stringify(req.body);
     try{
-      var json = require(`../babo/${req.body.sender}.json`);
+      var json1 = require(`../babo/${req.body.sender}.json`);
+      var json = require(`../babo/${req.body.receiver}.json`);
+      var tag = 0;
       console.log("있당");
-      json['data'].push(req.body);
+      json1['data'].push(req.body);
       // console.log(json);
-      for(var i=0; i<=json.friend.length; i++){
+      for(var i=0; i<=json1.friend.length; i++){
         console.log(i);
-        if(json.friend[i].id == req.body.receiver){
+        if(json1.friend[i].id == req.body.receiver){
           console.log("1111");
-          json.friend[i].flag = "0";
+          json1.friend[i].flag = "0";
+          tag = 1;
           break;
         }
       }
-      json = JSON.stringify(json);
+      if(tag==0){
+      json1.friend.push(json.user[0]);
+    }
+      json1 = JSON.stringify(json1);
       
-      fs.writeFile(`babo/${req.body.sender}.json`,json, (err) => {
+      fs.writeFile(`babo/${req.body.sender}.json`,json1, (err) => {
         if(err) throw err;
         console.log("넹");
         
       });
-      var json = require(`../babo/${req.body.receiver}.json`);
+      tag = 0
       json['data'].push(req.body);
       for(var i=0; i<=json.friend.length; i++){
         if(json.friend[i].id == req.body.sender){
           console.log("2222");
           json.friend[i].flag = "1";
+          tag = 1
           break;
         }
+      }
+      if(tag==0){
+       json.friend.push(json1.user[0]);
       }
       json = JSON.stringify(json);
       fs.writeFile(`babo/${req.body.receiver}.json`,json,(err)=>{
